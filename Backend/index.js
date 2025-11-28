566e07a24bc2974b38a4ee2a3a3c63c9db120af6
KrishnaGitHub18 <krishndeheriya1803@gmail.com>
2025-11-29 01:38:16 +0530

    db reconfigured

--- Backend/index.js
+++ Backend/index.js
@@ -1,37 +1,30 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import adminRoute from "./Routes/adminRoute.js"
-import clientRoute from "./Routes/clientRoute.js"
-import reagentRoute from "./Routes/reagentRoute.js"
-import clientOrder from "./Routes/clientOrder.js"
+import adminRoute from "./Routes/adminRoute.js";
+import clientRoute from "./Routes/clientRoute.js";
+import reagentRoute from "./Routes/reagentRoute.js";
+import clientOrder from "./Routes/clientOrder.js";
 import clientOrderDetails from "./Routes/clientOrderDetails.js";
 import verifyToken from "./Authorization/jwtmiddleware.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(express.json());
-// app.use(cors());
 app.use(cors());
+
 mongoose
-  .connect(
-    "mongodb+srv://krishnadeheriya1803:gIjr28fmAfMA0iOd@cluster0.tdkzmst.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }
-  )
-  .then(() => {
-    console.log("database is connnected");
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("database is connected"))
+  .catch((err) => console.log(err));
 
-  });
-  
-  app.use('/admin', adminRoute)
-  app.use('/client', clientRoute)
-  app.use('/reagent', verifyToken, reagentRoute)
-  app.use('/clientOrder',verifyToken ,clientOrder)
-  app.use('/clientorderDeatils' ,clientOrderDetails)
+app.use('/admin', adminRoute);
+app.use('/client', clientRoute);
+app.use('/reagent', verifyToken, reagentRoute);
+app.use('/clientOrder', verifyToken, clientOrder);
+app.use('/clientOrderDetails', clientOrderDetails);
 
-app.listen(3000, function () {
+app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
-
